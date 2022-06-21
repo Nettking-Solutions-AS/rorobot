a8d3d7a71e9b04b6a2d1b0e8758ed1e198af5088
AleksanderSandnes <62210498+AleksanderSandnes@users.noreply.github.com>
2022-06-21 15:56:21 +0200

    Sorting, icons and design

--- RobinRobot/components/Pages/Dashboard.tsx
+++ RobinRobot/components/Pages/Dashboard.tsx
@@ -2,7 +2,8 @@ import axios from "axios"
 import React, { useState, useEffect, useCallback } from "react";
 import { ScrollView, StyleSheet, ActivityIndicator } from "react-native";
 import { Table, Row, Rows } from "react-native-table-component"
-import { FontAwesome } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
+import { Text, HStack, Center, Box, Hidden } from "native-base"
 import { useGlobalState } from "../StateManagement/GlobalState";
 
 interface Balance {
@@ -21,8 +22,36 @@ interface Data {
 export default function Dashboard() {
   const { state } = useGlobalState();
   const [data, setData] = useState<Balance[]>([]);
-  const [loading, setLoading] = useState(false)
-  const [sortType, setSortType] = useState<"ascending" | "descending">("ascending")
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState<"ascending" | "descending">("ascending");
+  const [sortIconChange, setsortIconChange] = useState<"sort-asc" | "sort-desc" | "unfold">("unfold");
+
+  const amount = () => (
+    <HStack space={1}>
+      <Center mb="3" mr="2">
+        <Text bold style={styles.headerText}>Amount</Text>
+      </Center>
+      <Center>
+        <Octicons name={sortIconChange} size={22} style={styles.sortIcon} onPress={() => combinedOnPress()} />
+      </Center>
+    </HStack>
+  );
+
+  const token = () => (
+    <HStack space={1}>
+      <Center mb="3" mr="2">
+        <Text bold style={styles.headerTextFirstItem}>Token</Text>
+      </Center>
+      <Center>
+        <Octicons name="unfold" size={22} style={styles.sortIcon} />
+      </Center>
+    </HStack>
+  );
+
+  const combinedOnPress = () => {
+    setSortType(sortType === "ascending" ? "descending" : "ascending");
+    setsortIconChange(sortIconChange === "sort-asc" ? "sort-desc" : "sort-asc");
+  }
 
   const fetchData = useCallback(async () => {
     setLoading(true)
@@ -48,39 +77,63 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <ScrollView style={{}} contentContainerStyle={styles.container} >
-      {loading ? <ActivityIndicator size="large" /> : <Table>
-        <FontAwesome name="unsorted" size={32} style={styles.sortIcon} onPress={() => setSortType(sortType === "ascending" ? "descending" : "ascending")} />
-        <Row data={["Token", "Amount"]} style={styles.tableHeader} textStyle={[styles.headerText]} />
-        <Rows data={data.map(entry =>
-          [entry.asset, ((entry.free * 10 + entry.locked * 10) / 10).toFixed(8)]
-        )} textStyle={[styles.entry]} />
-      </Table>}
+    <ScrollView style={styles.background} contentContainerStyle={styles.container} >
+      <Box style={styles.containerBackground}>
+        {loading ? <ActivityIndicator size="large" /> : <Table>
+          <Row data={[token(), amount()]} style={styles.tableHeader} textStyle={[styles.headerText]} />
+          <Rows data={data.map(entry =>
+            [entry.asset, ((entry.free * 10 + entry.locked * 10) / 10).toFixed(8)]
+          )} textStyle={[styles.entry]} />
+        </Table>}
+      </Box>
     </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#111827"
+  },
   container: { 
     padding: 12 
   },
+  containerBackground: {
+    flexDirection: Row,
+    backgroundColor: "#222831",
+    borderWidth: 2,
+    padding: 20,
+    borderRadius: 15,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 4
+  },
   tableHeader: { 
-    borderWidth: 1, 
+    borderTopWidth: 1, 
+    borderBottomWidth: 1,
+    paddingTop: 20,
     padding: 12, 
-    backgroundColor: "#0000ff22", 
-    borderColor: "#0000ff44" 
+    borderColor: "#fff",
+    marginBottom: 10 
   },
-  headerText: { 
-    fontWeight: "bold" 
+  headerTextFirstItem: { 
+    color: "#fff",
   },
-  entry: { 
-    borderWidth: 1, 
-    padding: 4, 
+  headerText: {
+    color: "#fff",
+    marginLeft: 10
+  },
+  entry: {  
+    padding: 16, 
     backgroundColor: "#0000ff11", 
-    borderColor: "#0000ff44" 
+    borderColor: "#0000ff44",
+    color: "#fff",
+    fontSize: 15
   },
   sortIcon: { 
     alignSelf: "flex-end", 
-    marginBottom: 12 
+    marginBottom: 12,
+    color: "#fff"
   }
 })
