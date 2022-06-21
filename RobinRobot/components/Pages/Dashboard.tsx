660b6c06ed84ddaa2782d63636174a02cca297a6
AleksanderSandnes <62210498+AleksanderSandnes@users.noreply.github.com>
2022-06-21 10:13:50 +0200

    Table implementation

--- RobinRobot/components/Pages/Dashboard.tsx
+++ RobinRobot/components/Pages/Dashboard.tsx
@@ -1,22 +1,55 @@
 import axios from "axios"
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { ScrollView, Text, StyleSheet } from "react-native";
+import { Table, TableWrapper, Row, Col, Cell, Cols, Rows } from "react-native-table-component"
+
 import { useGlobalState } from "../StateManagement/GlobalState";
+
+interface Balance {
+  asset: string;
+  free: string;
+  locked: string;
+}
+
+interface Data {
+  result: {
+    balances: Balance[]
+  }
+}
 
 export default function Dashboard() {
   const { state } = useGlobalState();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Balance[]>([]);
+  const [loading, setLoading] = useState(false)
 
-  const fetchData = async () => {
+
+  const fetchData = useCallback(async () => {
+    setLoading(true)
     const uri = "https://robinserver.onrender.com";
-    const response = await axios.post(uri, { userID: state.currentUser.id });
-    setData(response.data);
-  }
+    const { data: response } = await axios.post<Data>(uri, { userID: state.currentUser.id });
+    setData(response.result.balances)
+    setLoading(false)
+  }, [])
 
   useEffect(() => {
-    fetchData();
+    (async () => {
+      await fetchData()
+    })()
   }, []);
 
   return (
-    
+    <ScrollView style={{}} contentContainerStyle={styles.container} >
+      <Table>
+        <Row data={["Token", "Amount"]} style={styles.tableHeader} textStyle={styles.headerText} />
+        <Rows data={data.map(entry => [entry.asset, parseFloat(entry.free) + parseFloat(entry.locked)])} textStyle={styles.entry} />
+      </Table>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { padding: 12 },
+  tableHeader: { borderWidth: 1, padding: 12, backgroundColor: "#0000ff22", borderColor: "#0000ff44" },
+  headerText: { fontWeight: "bold" },
+  entry: { borderWidth: 1, padding: 4, backgroundColor: "#0000ff11", borderColor: "#0000ff44" }
+})
