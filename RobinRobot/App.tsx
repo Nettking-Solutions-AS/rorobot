e2c0707c191da89ca2d1a26bba017c2e44bc4a92
Nettking <m@nettking.no>
2023-01-19 16:35:55 +0100

    Added docstring

--- RobinRobot/App.tsx
+++ RobinRobot/App.tsx
@@ -4,6 +4,11 @@ import { NativeBaseProvider } from "native-base";
 import GlobalStateProvider from "./components/StateManagement/GlobalState";
 import ScreenManager from "./navigators/ScreenManager";
 
+/**
+ * The main entry point for the application.
+ * Wraps the application with the GlobalStateProvider and NativeBaseProvider.
+ * @returns {JSX.Element} The JSX Element to render
+ */
 export default function App() {
   return (
     <GlobalStateProvider>
