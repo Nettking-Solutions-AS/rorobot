ff156005defe0d6cde00153b90ecef6e92393e19
Nettking <m@nettking.no>
2023-01-19 16:39:06 +0100

    added docstring

--- RobinRobot/screens/UserLogin.tsx
+++ RobinRobot/screens/UserLogin.tsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import Login from "../components/Pages/Login";
 import Registration from "../components/Pages/Registration";
 
+/**
+ * Component that renders either the Login or Registration page.
+ * Allows switching between the pages.
+ * @returns {JSX.Element} The JSX Element to render
+ */
 export default function UserLogin() {
   const [showLogin, setShowLogin] = useState(false);
   return showLogin ? (
